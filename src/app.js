1946f4eaeadf9582f9a43c63e2c4bb55144a9abc
Espla <37451532+alejandroespla@users.noreply.github.com>
2025-05-14 19:13:48 +0000

    New

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+   
+  let who = ['An old lady', 'A green dog', 'A flying elephant', 'Your mother', 'Pepito el de los palotes', 'A red snake'];
+  let action = ['ate', 'peed', 'crushed', 'broke', 'stole', ];
+  let what = ['my alarm clock', 'my car', 'my dignity', 'my time', 'my bike'];
+  let when = ['before going to the office', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was on my way', 'when I wasn´t expecting it'];
+  
+  let whoIndex = Math.floor(Math.random() * who.length);  
+  let actionIndex = Math.floor(Math.random() * action.length);  
+  let whatIndex = Math.floor(Math.random() * what.length);  
+  let whenIndex = Math.floor(Math.random() * when.length);  
+  
+  let excuseMe = `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;  
+  
+  document.getElementById("excuseMe").innerHTML = excuseMe;
 };
